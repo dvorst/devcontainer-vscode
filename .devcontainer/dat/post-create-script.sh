1858a56e5ab5fb9b950e8a79cd36caa45c2e9204
dvorst <87502756+dvorst@users.noreply.github.com>
2026-03-02 13:17:44 +0100

    feat: add git configuration to post-create-script

--- .devcontainer/dat/post-create-script.sh
+++ .devcontainer/dat/post-create-script.sh
@@ -14,3 +14,7 @@ sudo pre-commit install
 # make symbolic reference of .aws folder in user-dir to root dir
 # This makes the aws login of the user work with sudo commands, or commands run as root user
 sudo ln --force --symbolic --no-dereference "/home/$USER/.aws /root/.aws"
+
+# Configure git
+git config merge.ff false
+git config pull.rebase true
